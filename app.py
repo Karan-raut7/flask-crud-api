1685840c59ca9951fa8fef979a1acc91fc553680
Karan Kumar <invincibleraut@gmail.com>
2026-06-08 18:47:23 +0530

    Added validation for update and delete routes

--- app.py
+++ app.py
@@ -4,20 +4,31 @@ app = Flask(__name__)
 notes = []
 @app.route("/notes", methods=["POST"])
 def create_note():
+    try:
+        data = request.get_json()
+        if not data:
+            return {"error":"no JSON data provided"},400
+        
+        if "title" not in data:
+            return {"error": "Title is required"}, 400
+        notes.append(data)
 
-    data = request.get_json()
-    notes.append(data)
-
-    return {
+        return {
         "message": "Success"
-    }
+    },201
+    except Exception as e:
+        return{
+            "error":str(e)
+        },500
 @app.route("/notes", methods=["GET"])
 def get_notes():
     
    
-    return notes
+    return notes,200
 @app.route("/notes/<int:id>", methods=["DELETE"])
 def delete_note(id):
+    if id>= len(notes):
+        return {"error":"Note not found"},400
     notes.pop(id)
     return{
         "message": "note deleted"
@@ -25,14 +36,15 @@ def delete_note(id):
 
 @app.route("/notes/<int:id>", methods=["PUT"])
 def update_note(id):
+    if id>= len(notes):
+        return {"error":"note not found"},404
     data = request.get_json()
-
+    if not data:
+        return{"error":"no json data provided"},400
     notes[id] = data
     return {
         "message": "note updated"
     }
-@app.route("/")
-def home():
-    return "Server is alive"
+
 if __name__ == "__main__":
     app.run(debug=True)
